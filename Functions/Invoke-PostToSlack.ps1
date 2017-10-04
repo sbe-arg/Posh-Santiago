244ceb4bd6e176ab5092dbe5c2c37309c3e52286
Santiago Bernhardt <santiago.bernhardt@xero.com>
2017-10-05 08:32:55 +1300

    basic post to slack via hook function

---
+++ Functions/Invoke-PostToSlack.ps1
@@ -0,0 +1,11 @@
+﻿function Invoke-PostToSlack {
+    param(
+        [string]$url,
+        [string]$text
+
+    )
+    process{
+        $body = "{`"text`":`"$text - Sent from:$env:COMPUTERNAME-$env:USERNAME`"}"
+        Invoke-RestMethod -Uri $url -Body $Body -Method Post
+    }
+}
